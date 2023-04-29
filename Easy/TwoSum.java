20a5a3c7353862a174bc0611e73c21693af676a1
abdic <amel.abdic@student.reutlingen-university.de>
2023-04-29 20:41:10 +0200

    Initial Commit

---
+++ Easy/TwoSum.java
@@ -0,0 +1,25 @@
+package Easy;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int [] arr = {2,7,11,15};
+        int target = 9;
+        int res [] = twoSum(arr,target);
+        System.out.println("This is my Solution no copy pasterino:\n");
+        System.out.println(res[0] + " and " +res[1]);
+    }
+    public static int [] twoSum(int arr[], int target){
+        int sum = 0;
+        for(int i = 0; i < arr.length;i++ ){
+            for(int j = i+1; j<arr.length; j++){
+                
+                sum = arr[i]+arr[j];
+                if(sum == target)
+                {
+                    return new int [] {i,j};
+                }
+            }
+        }
+        return new int [] {-1,1}; 
+    }
+}
