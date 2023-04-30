5fb7be4dbb6396005135f4417d7c8ea3cc46696c
Amex221 <amel.abdic1999@gmail.com>
2023-04-30 23:00:19 +0200

    Added some nicer output for permutation task 1920

--- Easy/ArrayForPermutation.java
+++ Easy/ArrayForPermutation.java
@@ -3,8 +3,13 @@ public class ArrayForPermutation {
     public static void main(String[] args) {
         System.out.println("This is a question for array permutation \n");
         int [] arr ={0,2,1,5,3,4};
+        System.out.println("Old Array: \n");
+        for(int i = 0; i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
         ArrayForPermutation arrInput = new ArrayForPermutation();
         int [] res = arrInput.buildArray(arr);
+        System.out.println("New parmutation Array: \n");
         for(int i = 0; i< arr.length; i++){
             System.out.println(res[i]);
         }
