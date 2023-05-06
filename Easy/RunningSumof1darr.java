0664059dc99b64eae9ee644b1c42d2511c5b197f
Amex221 <amel.abdic1999@gmail.com>
2023-05-06 20:00:54 +0200

    Running Sum of 1D Array

---
+++ Easy/RunningSumof1darr.java
@@ -0,0 +1,21 @@
+package Easy;
+
+public class RunningSumof1darr {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        int[] runningSum = calculateRunningSum(nums);
+        for (int num : runningSum) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] calculateRunningSum(int[] nums) {
+        int[] runningSum = new int[nums.length];
+        runningSum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            runningSum[i] = runningSum[i - 1] + nums[i];
+        }
+        return runningSum;
+    }
+}
+
