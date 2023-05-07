78e70c99677f94b3d178666213a6161e51c30216
Amex221 <amel.abdic1999@gmail.com>
2023-05-07 22:20:26 +0200

    Maximum Number in Sentence added

---
+++ Easy/MaximumWordinSentence.java
@@ -0,0 +1,23 @@
+package Easy;
+
+class MaximumWordinSentence{
+public static void main(String[] args) {
+    String[] sentences = {"Das ist ein Satz.", "Hallo Welt", "Ich bin Amex"};
+    int maxWords = findMaxWords(sentences);
+    System.out.println("Maximum number of words in a single sentence: " + maxWords);
+}
+
+public static int findMaxWords(String[] sentences) {
+    int maxWords = 0;
+
+    for (String sentence : sentences) {
+        String[] words = sentence.split(" ");
+        int wordCount = words.length;
+        if (wordCount > maxWords) {
+            maxWords = wordCount;
+        }
+    }
+
+    return maxWords;
+}
+}
