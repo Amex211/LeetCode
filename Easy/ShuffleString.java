8c80f9a029aca432c5c09741615e7123e31f599c
Amex221 <amel.abdic1999@gmail.com>
2023-05-10 21:50:51 +0200

    ShuffleString

---
+++ Easy/ShuffleString.java
@@ -0,0 +1,19 @@
+package Easy;
+
+public class ShuffleString {
+    public static void main(String[] args) {
+        String s = "codeleet";
+        int[] indices = {4, 5, 6, 7, 0, 2, 1, 3};
+        String shuffled = restoreString(s, indices);
+        System.out.println(shuffled);
+    }
+    public static String restoreString(String s, int[] indices) {
+        char[] shuffled = new char[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            shuffled[indices[i]] = s.charAt(i);
+        }
+
+        return new String(shuffled);
+    }
+}
